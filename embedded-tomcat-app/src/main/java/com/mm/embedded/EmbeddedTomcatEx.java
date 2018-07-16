9483717501b2fb90f14d521d6bf6eb48cb23f383
Marek Czernek <mczernek@redhat.com>
2018-07-16 15:20:54 +0200

    Make paths windows ready

--- embedded-tomcat-app/src/main/java/com/mm/embedded/EmbeddedTomcatEx.java
+++ embedded-tomcat-app/src/main/java/com/mm/embedded/EmbeddedTomcatEx.java
@@ -19,12 +19,22 @@ public class EmbeddedTomcatEx {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
         tomcat.setBaseDir(".");
-	tomcat.getConnector();
+	    tomcat.getConnector();
 
         //Context ctx = tomcat.addWebapp("/clusterbench", "/tmp/server/clusterbench.war");
         Context ctx, ctx2;
-        ctx = tomcat.addWebapp("/jaxrs-server", "/tmp/cxf-embedded-tomcat-toolset/jaxrs-server.war");
-        ctx2 = tomcat.addWebapp("/jaxws-server", "/tmp/cxf-embedded-tomcat-toolset/jaxws-server.war");
+
+        String path = System.getProperty("java.io.tmpdir");
+        if (path == null) {
+            if (System.getProperty("os.name").contains("win")) {
+                path = "C:\\tmp\\";
+            } else {
+                path = "/tmp/cxf-embedded-tomcat-toolset/";
+            }
+        }
+        
+        ctx = tomcat.addWebapp("/jaxrs-server", path + "jaxrs-server.war");
+        ctx2 = tomcat.addWebapp("/jaxws-server", path + "jaxws-server.war");
         
         //ctx.addServletMapping("/*", "Embedded");
 
